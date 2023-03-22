98d256c2689d3b9814f126d03ca1d658e6027f13
archana <archanavadde88@gmail.com>
2023-03-22 22:15:08 +0000

    vv

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -87,7 +87,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             owner = "AWS"
             input_artifacts = ["tf-code"]
             configuration = {
-                ProjectName = "tf-cicd-plan"
+                ProjectName = "tf-cicd-plan2"
             }
         }
     }
