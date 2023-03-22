d507e79509e086728d8a0186aa5930e48fedd486
archana <archanavadde88@gmail.com>
2023-03-22 21:55:30 +0000

    new

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -70,7 +70,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             output_artifacts = ["tf-code"]
             configuration = {
                 FullRepositoryId = "archanavadde88/aws-cicd-pipeline"
-                BranchName   = "master"
+                BranchName   = "main"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
             }
