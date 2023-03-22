2519229a7fbabe87925ba169d3e735f2ce13a09d
archana <archanavadde88@gmail.com>
2023-03-22 22:01:16 +0000

    kk

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -1,4 +1,11 @@
 resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "pipeline-artifacts-dammo"
-  acl    = "private"
+  
 } 
+ resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.codepipeline_artifacts.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  }
